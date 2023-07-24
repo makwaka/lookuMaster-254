e6bcbc4ef726b28ea60db4568c5a8e774e6059ce
Phariq <pharisnjuguna22@gmail.com>
2023-07-24 17:22:54 +0300

    Added styles to the navbar, added margin to the footer

--- src/footer.js
+++ src/footer.js
@@ -8,7 +8,7 @@ function footer() {
     </h5>
     <a  href='tel: O706822662'>O706822662</a> <p> OR </p>
     <a  href='tel: O775858585'>O775858585</a>
-    <p className='web-by'>Website by fariq</p>
+    <a href='pharisnjuguna22@gmail.com'><p className='web-by'>Website by fariq</p></a>
 
   </article>
     </div>
